9834397d2ea679708b4fc246380e431c37f10f65
Austin <joha0033@gmail.com>
2017-10-18 14:17:52 -0600

    first things first, empty works

---
+++ Format_to_String_of_Names/app.js
@@ -0,0 +1,14 @@
+function list(names){
+
+  if(names[0] === undefined){
+    console.log('ok!');
+    return []
+  }
+
+}
+
+// list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}])
+// list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}])
+// list([{name: 'Bart'},{name: 'Lisa'}])
+// list([{name: 'Bart'}])
+list([])

f5f3fa0613c36bba8c9c435b21dfcd9011cb4518
Austin <joha0033@gmail.com>
2017-10-19 11:42:59 -0600

    format_String no name and 1 name working

--- Format_to_String_of_Names/app.js
+++ Format_to_String_of_Names/app.js
@@ -1,14 +1,18 @@
 function list(names){
 
   if(names[0] === undefined){
-    console.log('ok!');
-    return []
+    console.log('');
+    return ''
+  }else if(names[0] != undefined && names.length <= 1){
+    console.log(names[0].name);
+    return
   }
+  
 
 }
 
 // list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}])
 // list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}])
-// list([{name: 'Bart'},{name: 'Lisa'}])
-// list([{name: 'Bart'}])
+list([{name: 'Bart'},{name: 'Lisa'}])
+list([{name: 'Bart'}])
 list([])
